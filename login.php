3125700f5e2fe9365b9e0a396c1b3c631efd797e
arunaS <aroona.shrestha@gmail.com>
2016-10-01 22:27:03 +0545

    comment username

--- login.php
+++ login.php
@@ -1,3 +1,6 @@
+<!-- username:aruna
+password:aruna -->
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,6 +11,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
  <!--  <a href="#" role="button" class="btn btn-info active">Signup</a> -->
+
 </head>
 <body>
 <div class="container">
